b335e5d47605045d5260b99093bfa7082086108d
zhangyuhao26 <zhangyuhao26@jd.com>
2021-09-03 17:53:22 +0800

    增加requirements.txt文件

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ data_dir = cwd + '/data/'
 save_dir = cwd + '/save/'
 
 #Transformer 的基础模型使用的数值为：num_layers=6，d_model = 512，dff = 2048
-#为了让本示例小且相对较快，已经减小了num_layers、 d_model 和 dff 的值。
 num_layers = 4
 d_model = 128
 dff = 512
@@ -20,5 +19,6 @@ num_heads = 8
 dropout_rate = 0.1
 MAX_LENGTH = 10
 BATCH_SIZE = 64 * ngpu
-EPOCHS = 40 # 50 # 30  # 20
-print_trainstep_every = 50  # 每50个step做一次打印
+EPOCHS = 40
+# 每50个step做一次打印
+print_trainstep_every = 50
